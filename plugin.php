8cc4b559c3856b29de0be0f40dad91e7e1a1d364
Fred Bradley <hello@fredbradley>
2015-07-29 04:04:36 -0700

    Added description text to plugin page

--- plugin.php
+++ plugin.php
@@ -205,7 +205,11 @@ class TheSwipeCookieWidget {
 					<a class="center" href="http://swipe.digital">
 						<img src="http://swipe.digital/wp-content/uploads/2015/06/swipe_logo_site1.png" class="center" />
 					</a>
-					<p>Hello there is this interesting isn't it. Hello there is this interesting isn't it.</p>
+					<p>European laws require that digital publishers give visitors to their sites and apps information about their use of cookies and other forms of local storage. In many cases these laws also require that consent be obtained.</p>
+
+<p>Swipe Digital have created a helpful snippet of code that will automatically place a bar at the bottom of your site informing visitors of the use of Cookies. When a user clicks "OK", a cookie is set for 90 days and the bar disappears for that time.</p>
+
+<p>Use of this plugin is free. If you require an unbranded version of this plugin, please feel free get in contact with the developer.</p>
 				</div>
 			</div>
 		</div>
